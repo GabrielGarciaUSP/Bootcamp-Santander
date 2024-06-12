442163acc60c69d94eca51ca720f2dda65a8728d
Gabriel Garcia <gabrielgarcia.ata@gmail.com>
2024-06-12 15:23:44 -0300

    Finalização dos Fundamentos de POO

---
+++ Programação Orientada a Objetos/Fundamentos De POO/src/Pessoas/SistemaCadastro.java
@@ -0,0 +1,13 @@
+package Pessoas;
+
+public class SistemaCadastro {
+	public static void main(String[] args) {
+		//criamos uma pessoa no sistema
+		Pessoa marcos = new Pessoa("123","Marcos");
+		
+		//definimos o endereço de marcos
+		marcos.setEndereco("RUA DAS MARIAS");
+		
+		System.out.println(marcos.getNome() + "-" + marcos.getCpf());
+	}
+}
