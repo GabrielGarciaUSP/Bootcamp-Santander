cf6cdc5c772a1ab0e1775d8c869090efd55c4514
Gabriel Garcia <gabrielgarcia.ata@gmail.com>
2024-06-14 14:06:44 -0300

    3º Pilar - Abstração

--- Programação Orientada a Objetos/Pilares de POO/ServiçoMensagemInstantanea/src/MSNMessenger.java
+++ Programação Orientada a Objetos/Pilares de POO/ServiçoMensagemInstantanea/src/MSNMessenger.java
@@ -1,16 +1,8 @@
-public class MSNMessenger extends ServicoMensagemInstantanea {
-  /*public void enviarMensagem() {
-		validarConectadoInternet();
+public class MSNMessenger extends ServicoMensagemInstantanea{
+	public void enviarMensagem() {
 		System.out.println("Enviando mensagem pelo MSN Messenger");
-		salvarHistoricoMensagem();
 	}
 	public void receberMensagem() {
 		System.out.println("Recebendo mensagem pelo MSN Messenger");
 	}
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a internet");
-	}
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-	}*/
 }
--- Programação Orientada a Objetos/Pilares de POO/ServiçoMensagemInstantanea/src/Telegram.java
+++ Programação Orientada a Objetos/Pilares de POO/ServiçoMensagemInstantanea/src/Telegram.java
@@ -1,16 +1,8 @@
-public class Telegram extends ServicoMensagemInstantanea{
-	/*public void enviarMensagem() {
-		validarConectadoInternet();
+public class Telegram extends ServicoMensagemInstantanea {
+	public void enviarMensagem() {
 		System.out.println("Enviando mensagem pelo Telegram");
-		salvarHistoricoMensagem();
 	}
 	public void receberMensagem() {
 		System.out.println("Recebendo mensagem pelo Telegram");
 	}
-	private void validarConectadoInternet() {
-		System.out.println("Validando se está conectado a internet");
-	}
-	private void salvarHistoricoMensagem() {
-		System.out.println("Salvando o histórico da mensagem");
-    } */
 }
