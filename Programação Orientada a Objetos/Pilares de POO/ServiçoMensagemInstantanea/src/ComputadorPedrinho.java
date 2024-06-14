c12b5eee0d2d0376ff3c923a9f52cc88a4c0c697
Gabriel Garcia <gabrielgarcia.ata@gmail.com>
2024-06-14 14:22:21 -0300

    4º Pilar - Polimorfismo

--- Programação Orientada a Objetos/Pilares de POO/ServiçoMensagemInstantanea/src/ComputadorPedrinho.java
+++ Programação Orientada a Objetos/Pilares de POO/ServiçoMensagemInstantanea/src/ComputadorPedrinho.java
@@ -1,18 +1,21 @@
 public class ComputadorPedrinho {
-   public static void main(String[] args) {
-       System.out.println("MSN Messenger");
-       MSNMessenger msn = new MSNMessenger();
-       msn.enviarMensagem();
-       msn.receberMensagem();
-
-       System.out.println("Facebook Messenger");
-       FacebookMessenger fcbk = new FacebookMessenger();
-       fcbk.enviarMensagem();
-       fcbk.receberMensagem();
-       
-       System.out.println("Telegram");
-       Telegram tlg = new Telegram();
-       tlg.enviarMensagem();
-       tlg.receberMensagem();
-   } 
-}   
+	public static void main(String[] args) {
+		
+		ServicoMensagemInstantanea smi = null;
+		//String appEscolhido define com qual app eu irei enviar a mensagem
+		String appEscolhido="msn"; 
+		
+		if(appEscolhido.equals("msn"))
+			smi = new MSNMessenger();
+		else if(appEscolhido.equals("fbm"))
+			smi = new FacebookMessenger();
+		else if(appEscolhido.equals("tlg"))
+			smi = new Telegram();
+		
+			
+		smi.enviarMensagem();
+		smi.receberMensagem();
+        
+        smi.validarConectadoInternet();
+	}
+}
--- Programação Orientada a Objetos/Pilares de POO/ServiçoMensagemInstantanea/src/MSNMessenger.java
+++ Programação Orientada a Objetos/Pilares de POO/ServiçoMensagemInstantanea/src/MSNMessenger.java
@@ -1,5 +1,6 @@
 public class MSNMessenger extends ServicoMensagemInstantanea{
 	public void enviarMensagem() {
+		validarConectadoInternet();
 		System.out.println("Enviando mensagem pelo MSN Messenger");
 	}
 	public void receberMensagem() {
